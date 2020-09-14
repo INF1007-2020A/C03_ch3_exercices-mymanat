797f09f4cf4d3fceab6d5e9a1c1308695628916b
mymanat <71021952+mymanat@users.noreply.github.com>
2020-09-14 14:25:54 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -6,7 +6,8 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    mean=(a+b+c)/3
+    return mean
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
